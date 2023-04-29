c83c09a535b438fd526ae14ffdc42da55eda3c64
jzamorano20 <126306433+jzamorano20@users.noreply.github.com>
2023-04-29 10:41:02 -0400

    add

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,9 +49,9 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const updated = await Category.update(req.body, { where: { id: req.params } });
+    const updated = await Category.update(req.body, { where: { id: req.params.id } });
     !updated[0] ? res.status(404).json({ message: "not found id" }):
-    res.status(200).json(update);
+    res.status(200).json(updated);
   } catch (err) {
     res.status(500).json({ message: 'update failed' });
   }
